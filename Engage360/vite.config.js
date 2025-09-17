b2537adca9e23e801f34dad7900bab9ac6bfe782
snat0022 <snat0022@student.monash.edu>
2025-09-17 15:11:00 +1000

    made few more changes

--- Engage360/vite.config.js
+++ Engage360/vite.config.js
@@ -1,18 +1,19 @@
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import path from "path"
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    port: 5173, // optional: ensures consistency locally
+    open: true,
+  },
+  build: {
+    outDir: "dist", // Cloudflare expects dist
   },
 })
